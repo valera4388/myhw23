197974c4dd690b98974020dea6371d29960af5c3
mikhalev valera <mihalev.valera@mail.ru>
2024-11-16 22:02:16 +0300

    add test

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -31,14 +31,6 @@ public class DataHelper {
         return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
 
-//    public static int generateValidAmount(int balance) {
-//        return Math.abs(balance) / 10;
-//    }
-//
-//    public static int generateInvalidAmount(int balance) {
-//        return Math.abs(balance) + 1;
-//    }
-
     @Value
     public static class VerificationCode {
         private String code;
